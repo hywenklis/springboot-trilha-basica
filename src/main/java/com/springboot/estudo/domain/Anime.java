e4b08f51e982029ee0215aee35b4101d35eef0db
hywenklis <hywenklis@hotmail.com>
2021-12-02 15:52:47 -0300

    Repositorio de estudo spring boot

---
+++ src/main/java/com/springboot/estudo/domain/Anime.java
@@ -0,0 +1,12 @@
+package com.springboot.estudo.domain;
+
+public class Anime {
+    private String name;
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public Anime() {
+    }
+}
