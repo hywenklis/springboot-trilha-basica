09475b65ef6e832d17f3115cd74ffa8e8d4caa0b
hywenklis <hywenklis@hotmail.com>
2021-12-23 23:21:49 -0300

    Utilizando o log e pacote util, usando injeção de dependencia apartir do construtor com lombok

--- src/main/java/com/springboot/estudo/controller/AnimeController.java
+++ src/main/java/com/springboot/estudo/controller/AnimeController.java
@@ -1,18 +1,28 @@
 package com.springboot.estudo.controller;
 
 import com.springboot.estudo.domain.Anime;
+import com.springboot.estudo.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("/anime")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
+
+    private final DateUtil dateUtil;
 
     @GetMapping("/list")
     public List<Anime> list() {
+
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("Naruto"), new Anime("Berserk"));
     }
 }
