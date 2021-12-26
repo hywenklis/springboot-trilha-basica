f9259bd40fa10456d8cb6f5910b9444a01919d90
hywenklis <hywenklis@hotmail.com>
2021-12-26 12:51:50 -0300

    Implementando metodo findById e utilizando HttpStatus para respostas com ResponseEntitiy

--- src/main/java/com/springboot/estudo/service/AnimeService.java
+++ src/main/java/com/springboot/estudo/service/AnimeService.java
@@ -1,14 +1,25 @@
 package com.springboot.estudo.service;
 
 import com.springboot.estudo.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
 
+    private final List<Anime> animes = List.of(new Anime(1L,"Naruto"), new Anime(2L,"Berserk"));
+
     public List<Anime> listAll() {
-        return List.of(new Anime(1L,"Naruto"), new Anime(2L,"Berserk"));
+        return animes;
+    }
+
+    public Anime findbyId(Long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found!"));
     }
 }
